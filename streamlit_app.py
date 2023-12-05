2fc81b2b5dc2145ae0ebcff8c6e366e931d6c7d2
Ben Kuhn <bn.kuhn1@gmail.com>
2023-12-05 09:52:20 -0700

    showing name, type, and who they provide services for

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+def markdown_for_service(service: str):
+    return f"**{service.title()}:** {' :white_check_mark:' if row[service] == 'yes' else ':x:'}"
+
 # Load the CSV data
 csv_path = 'TEM_providers.csv'  # Replace with the path to your CSV file
 df = pd.read_csv(csv_path)
@@ -12,7 +15,28 @@ st.title('TEM Finder')
 st.write('## Raw Data')
 st.dataframe(df)
 
+for index, row in df.iterrows():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Name")
+        st.markdown(f"[{row['Name']}]({row['Website']})")
 
+    with col2:
+        st.header("Type")
+        st.text(row['Type of institution'])
+
+      
+    with col3:
+        st.header("Serving")
+        # write markdown for the research category where if it is yes it shows a check emoji and if it is no it shows a cross emoji
+        st.markdown(markdown_for_service('Research'))
+        st.markdown(markdown_for_service('Clinical'))
+
+        # refactor the markdown above to be a function that takes in a string and returns the markdown
+        
+
+
+    st.write('---')
 
 # # Filtering options
 # st.sidebar.title('Filter Options')
