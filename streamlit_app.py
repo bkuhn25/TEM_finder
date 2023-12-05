35f584d525a03fee50ec225c1360a882f245b157
Ben Kuhn <bn.kuhn1@gmail.com>
2023-12-05 11:05:00 -0700

    Let's call this functional but not styled

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,22 +88,3 @@ for index, row in filtered_df.iterrows():
 
     st.write('---')
 
-# # Add filtering widgets based on column types
-# for column in df.columns:
-#     filter_options = st.sidebar.multiselect(f'Select {column}:', df[column].unique())
-#     if filter_options:
-#         df = df[df[column].isin(filter_options)]
-
-# # Display filtered data
-# st.write('## Filtered Data')
-
-# # Display filtered data with custom formatting
-# for index, row in df.iterrows():
-#     st.write('### Name:', row['Name'])
-#     st.write('**Type of institution:**', row['Type of institution'])
-#     st.write('**Services:**', row['Services'], unsafe_allow_html=True)
-#     st.write('**Instruments:**', row['Instruments'])
-#     st.write('**Website:**', row['Website'])
-#     st.write('**Contact:**', row['Contact'], unsafe_allow_html=True)
-#     st.write('**How to get a quote:**', row['How to get a quote'])
-#     st.write('---')
